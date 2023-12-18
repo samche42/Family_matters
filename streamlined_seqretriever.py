87f8838ee9234c7923a2779d732d6978616c1273
Sam Waterworth <60663840+samche42@users.noreply.github.com>
2023-12-18 10:58:48 -0500

    Update streamlined_seqretriever.py

--- streamlined_seqretriever.py
+++ streamlined_seqretriever.py
@@ -6,19 +6,28 @@ from Bio import SeqIO
 wanted =[]
 
 input_directory = sys.argv[1]
-#directory is path to orthologous group files
+#directory is path to multifastas
 nucleotide_file = sys.argv[2]
 #nucleotide_file is path the conactenated nucleotide file corresponding to proteins
 
-protein_file_list = [os.path.abspath(fn) for fn in glob(input_directory+'/*multifasta.aln')]
-#creation of list that hold fullpath to each OG file to be parsed
+multifasta_file_list = [os.path.abspath(fn) for fn in glob(input_directory+'/*multifasta.aln')]
+#creation of list that hold fullpath to each multifasta file to be parsed
+print(str(multifasta_file_list))
 
-for OG_group in protein_file_list:
-        output_file = (os.path.abspath(OG_group))[:-4]+"_nuc.fasta"
-        #outputfile named the same as input OG file
-        wanted = list(r.id for r in SeqIO.parse(OG_group, "fasta"))
-        #lists all headers in OG file
-        records = (r for r in SeqIO.parse(nucleotide_file, "fasta") if r.id in wanted)
-        #pulls all fastas with headers that correspond to wanted list
+for file in multifasta_file_list:
+        records = []
+        wanted = []
+        output_file = os.path.abspath(file).split(".")[0]+"_nuc.fasta"
+        print(os.path.abspath(output_file))
+#       outputfile named the same as input OG file
+        wanted = list(r.id for r in SeqIO.parse(file, "fasta"))
+        print(str(wanted))
+#       lists all headers in OG file
+        for item in wanted:
+                for r in SeqIO.parse(nucleotide_file, "fasta"):
+                        if r.id == item:
+                                print(str(r.id))
+                                records.append(r)
+#       pulls all fastas with headers that correspond to wanted list
         SeqIO.write(records, output_file, "fasta")
-        #writes pulled fastas to output files
+#       writes pulled fastas to output files
